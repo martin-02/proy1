22ce46d23ae6e65783bea9d4b310e5f1212272e6
Martin-02 <martinberlanas2002@gmail.com>
2021-03-01 13:54:56 -0800

    comentarios

--- src/Aplicacion/Principal.java
+++ src/Aplicacion/Principal.java
@@ -2,6 +2,10 @@ package aplicacion;
 
 import mates.Matematicas;
 
+/*
+ * Esta clase implementa al método Principal.
+ * El argumento "args[0] indica el número de lanzamientos que el programa va a realizar. 
+  */
 public class Principal {
 
     public static void main(String[] args) {
--- src/Matematicas/mates/Matematicas.java
+++ src/Matematicas/mates/Matematicas.java
@@ -1,10 +1,24 @@
+Copyright [2021] [Martin Berlanas Molera]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
 package mates;
 
 import java.util.Random;
 
 public class Matematicas {
 
-    /**
+    /*
     * Genera una aproximación al número pi mediante el método de Montecarlo.
     * El parámetro ‘pasos‘ indica el número de puntos generado.
     */
@@ -16,7 +30,7 @@ public class Matematicas {
         int total = 0;
         int dentroCirculo = 0;
 
-        //
+        //Método que genera una serie de números aleatorios para calcular el número Pi mediante MonteCarlo.
         double r = 0.5;
         Random rand = new Random();
         for (int i = 0; i < numeroIntentos; i++) {
@@ -30,7 +44,8 @@ public class Matematicas {
 
             total++;
         }
-            
+
+	//Calculamos la aproximación a Pi mediante los métodos anteriores y devuelve el resultado (el cual se va actualizando)
         pi = (long) (16 * ((double) dentroCirculo / (double) total));
         return pi;
     }
