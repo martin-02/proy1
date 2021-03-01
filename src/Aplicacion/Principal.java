d7597d4bcc5dd891f136b9dfd1100a6d4b8dfde6
Martin-02 <martinberlanas2002@gmail.com>
2021-03-01 12:56:35 -0800

    correcciones

--- src/Aplicacion/Principal.java
+++ src/Aplicacion/Principal.java
@@ -7,5 +7,5 @@ public class Principal {
     public static void main(String[] args) {
         long numeroIntentos = Long.parseLong(args[0]);
         System.out.print("Pi es: " + Matematicas.calcularPi(numeroIntentos));
-    }
+	}
 }
